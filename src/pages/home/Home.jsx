7700bc38842219fa0c551ff5aed9e601209a94ca
eftekher-alam <eftekher@yahoo.com>
2023-10-06 12:43:11 +0600

    Layout preparing 50% done

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../shared/Header";
+import NavBar from "../shared/NavBar";
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <NavBar></NavBar>
+            this is home page
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/shared/NavBar.jsx
@@ -0,0 +1,10 @@
+
+const NavBar = () => {
+    return (
+        <div>
+            this is Nav Bar
+        </div>
+    );
+};
+
+export default NavBar;
